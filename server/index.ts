adc9c9f16348776395d4e508876c4a402b06a6a6
Hoang Duong <hoangduongphantri@gmail.com>
2025-07-21 22:16:24 +0000

    fix: Modify the workflow

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,29 @@
 import { ContextRetriever } from "./tools";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
-import { WorkFlow } from "./workflow";
+import { Workflow } from "./workflow";
 import { MemorySaver } from "@langchain/langgraph";
 import { HumanMessage } from "@langchain/core/messages";
 
 const contextRetriever = new ContextRetriever(
-    "https://www.typescriptlang.org/docs/handbook/"
+  "https://www.typescriptlang.org/docs/handbook/"
 );
 const tools = [contextRetriever];
 const toolNode = new ToolNode(tools);
 
-const newWorkflow = WorkFlow(toolNode);
+const newWorkflow = Workflow(tools, toolNode);
 const checkPointer = new MemorySaver();
 
 const app = newWorkflow.compile({ checkpointer: checkPointer });
 
-const final_messages = app.invoke({
+const finalMessages = app.invoke(
+  {
     messages: [
-        new HumanMessage({ content: "What are TypeScript primitive types" }),
+      new HumanMessage({ content: "What are TypeScript primitive types" }),
     ],
-});
+  },
+  {
+    configurable: { thread_id: 42 },
+  }
+);
 
-console.log(final_messages["messages"][-1].content)
+console.log(finalMessages["messages"]);
--- server/workflow.ts
+++ server/workflow.ts
@@ -1,41 +1,51 @@
-import { END, START, StateGraph, MessagesAnnotation, Annotation } from "@langchain/langgraph"
-import { ContextRetriever } from "./tools"
-import { ChatOpenAI } from "@langchain/openai"
-import { ToolNode } from "@langchain/langgraph/prebuilt"
+import 'dotenv/config';
+import {
+  END,
+  START,
+  StateGraph,
+  MessagesAnnotation,
+  Annotation,
+} from "@langchain/langgraph";
+import { ContextRetriever } from "./tools";
+import { ChatOpenAI } from "@langchain/openai";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 
-export const WorkFlow = (toolNode: ToolNode) => {
-    const shouldContinueWorkflow = (state: typeof MessagesAnnotation.State) => {
-        const messages = state.messages
-        const lastMessage = messages[messages.length - 1]
-        if (lastMessage && "toolCalls" in lastMessage)
-            return "tools"
-        return END
-    }
-    
-    const model = (tools: ContextRetriever[]) => {
-        return new ChatOpenAI({model: "gpt-4o-mini", temperature: 0}).bindTools(tools)
-    } 
-    
-    const callModel = async (state: typeof MessagesAnnotation.State, tools: ContextRetriever[]) => {
-        const messages = state.messages
-        const newModel = model(tools)
-        const response = await newModel.invoke(messages)
-        return {"messages": [response]}
-    }
-    
-    const GraphAnnotation = Annotation.Root({
-        language: Annotation<string>(),
-        ...MessagesAnnotation.spec,
-      });
-    
-    const workflow = new StateGraph(GraphAnnotation)
-    
-    workflow.addNode("agent", callModel)
-    workflow.addNode("tools", toolNode)
+const apiKey: string = process.env.OPENAI_API_KEY || "";
 
-    workflow.addEdge(START, "agent")
-    workflow.addConditionalEdges("agent", shouldContinueWorkflow)
-    workflow.addEdge("tools", "agent")
+export const Workflow = (
+  tools: ContextRetriever[],
+  toolNode: ToolNode<any>
+) => {
+  const shouldContinueWorkflow = (state: typeof MessagesAnnotation.State) => {
+    const messages = state.messages;
+    const lastMessage = messages[messages.length - 1];
+    if (lastMessage && "toolCalls" in lastMessage) return "tools";
+    return END;
+  };
 
-    return workflow
-}
+  const model = new ChatOpenAI({
+    model: "gpt-4o-mini",
+    temperature: 0,
+    apiKey: apiKey,
+  }).bindTools(tools);
+
+  const callModel = async (state: typeof MessagesAnnotation.State) => {
+    const messages = state.messages;
+    const response = await model.invoke(messages);
+    return { messages: [response] };
+  };
+
+  const GraphAnnotation = Annotation.Root({
+    language: Annotation<string>(),
+    ...MessagesAnnotation.spec,
+  });
+
+  const workflow = new StateGraph(GraphAnnotation)
+    .addNode("agent", callModel)
+    .addNode("tools", toolNode)
+    .addEdge(START, "agent")
+    .addConditionalEdges("agent", shouldContinueWorkflow)
+    .addEdge("tools", "agent");
+
+  return workflow;
+};
