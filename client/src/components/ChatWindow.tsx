4969102fc8e337f206f35f35043b75b662ade3e0
hoangduong-coder <hoangduongphantri@gmail.com>
2025-08-09 17:03:49 +0300

    fix: Modify the input and chat styling

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useRef } from "react";
 import type { Messages } from "../types";
 import MessageCard from "./MessageCard";
 
 const ChatWindow = ({ messages }: { messages: Messages[] }) => {
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
-    <div className="chat-window-body">
+    <div className="chat-window-body" ref={messagesEndRef}>
       {messages.map((msg, idx) => (
         <MessageCard key={idx} message={msg} />
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
